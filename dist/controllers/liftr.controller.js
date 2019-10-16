128e620977761c472fe764e8de3c78cf92e5d550
Faris Tangastani <ftangastani@gmail.com>
2019-10-16 12:55:48 +0200

    removed dist folder

--- dist/controllers/liftr.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function liftrController(req, res) {
-    res.send('Lift off!');
-}
-exports.liftrController = liftrController;
-;
--- dist/schema/liftr.schema.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@liftr/core");
-exports.liftrRouteschema = core_1.Joi.object().keys({
-    message: core_1.Joi.string().required(),
-});
